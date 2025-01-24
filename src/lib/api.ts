28fdd9be0cb575978cae0e705bed1c0392614aea
pain-arch <himulol123@gmail.com>
2025-01-24 21:21:14 +0600

    implement registration functionality with mutation and handle navigation on success

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,12 +1,12 @@
 import API from "./axios-client";
-import { CurrentUserResponseType, LoginResponseType, loginType } from "@/types/api.type";
+import { CurrentUserResponseType, LoginResponseType, loginType, registerType } from "@/types/api.type";
 
 export const loginMutationFn = async (data: loginType):Promise<LoginResponseType> => {
   const response = await API.post("/auth/login", data);
   return response.data;
 };
 
-export const registerMutationFn = async () => {};
+export const registerMutationFn = async (data: registerType) => await API.post("/auth/register", data);
 
 export const logoutMutationFn = async () => {};
 
