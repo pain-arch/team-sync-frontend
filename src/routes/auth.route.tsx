0f34d633f71e1d22a676303cd1408e79d2a1c0b9
pain-arch <himulol123@gmail.com>
2025-01-27 17:21:36 +0600

    enhance authentication routes with loading state and user redirection

--- src/routes/auth.route.tsx
+++ src/routes/auth.route.tsx
@@ -1,7 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { DashboardSkeleton } from "@/components/skeleton-loaders/dashboard-skeleton";
+import useAuth from "@/hooks/api/use-auth";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { isAuthRoute } from "./common/routePaths";
 
 const AuthRoute = () => {
-  return <Outlet />;
+
+  const locattion = useLocation();
+
+  const { data: authData, isLoading} = useAuth();
+  const user = authData?.user;
+
+  const _isAuthRoute = isAuthRoute(locattion.pathname);
+
+  if (isLoading && !_isAuthRoute) return <DashboardSkeleton />;
+
+  if (!user) return <Outlet />;
+  return <Navigate to={`workspace/${user.currentWorkspace?._id}`} replace/>;
 };
 
 export default AuthRoute;
