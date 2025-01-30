2f28a61fa62a217d1948e74cd96fb0c4a540f86e
pain-arch <himulol123@gmail.com>
2025-01-30 21:46:19 +0600

    enhance authentication context to include workspace data and permissions management

--- src/context/auth-provider.tsx
+++ src/context/auth-provider.tsx
@@ -1,15 +1,24 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { createContext, useContext, useEffect } from "react";
-// import useWorkspaceId from "@/hooks/use-workspace-id";
+import useWorkspaceId from "@/hooks/use-workspace-id";
 import useAuth from "@/hooks/api/use-auth";
-import { UserType } from "@/types/api.type";
+import { UserType, WorkspaceType } from "@/types/api.type";
+import useGetWorkspaceQuery from "@/hooks/api/use-get-workspace";
+import { useNavigate } from "react-router-dom";
+import usePermissions from "@/hooks/use-permissions";
+import { PermissionType } from "@/constant";
 
 // Define the context shape
 type AuthContextType = {
   user?: UserType;
+  workspace?: WorkspaceType;
+  hasPermission: (permission: PermissionType) => boolean;
   error: any;
   isLoading: boolean;
   isFetching: boolean;
+  workspaceLoading: boolean;
   refetchAuth: () => void;
+  refetchWorkspace: () => void;
 };
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
@@ -17,29 +26,53 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  
+  const navigate = useNavigate();
+  const workspaceId = useWorkspaceId();
+
   const {
     data: authData,
     error: authError,
-    isLoading: authLoading,
+    isLoading,
     isFetching,
     refetch: refetchAuth,
   } = useAuth();
-
   const user = authData?.user;
 
-  // const workspaceId = useWorkspaceId();
+  const {
+    data: workspaceData,
+    isLoading: workspaceLoading,
+    error: workspaceError,
+    refetch: refetchWorkspace,
+  } = useGetWorkspaceQuery(workspaceId);
 
-  useEffect(() => {});
+  const workspace = workspaceData?.workspace;
+
+  useEffect(() => {
+    if (workspaceError) {
+      if (workspaceError?.errorCode === "ACCESS_UNAUTHORIZED") {
+        navigate("/"); // Redirect if the user is not a member of the workspace
+      }
+    }
+  }, [navigate, workspaceError]);
+
+  const permissions = usePermissions(user, workspace);
+
+  const hasPermission = (permission: PermissionType): boolean => {
+    return permissions.includes(permission);
+  };
 
   return (
     <AuthContext.Provider
       value={{
         user,
-        error: authError,
-        isLoading: authLoading,
+        workspace,
+        hasPermission,
+        error: authError || workspaceError,
+        isLoading,
         isFetching,
+        workspaceLoading,
         refetchAuth,
+        refetchWorkspace,
       }}
     >
       {children}
